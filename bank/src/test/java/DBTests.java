6c3b2c89191a8bee0626cf05f482940fa683346c
Unknown <s.vanmaurik@student.fontys.nl>
2017-11-13 15:00:09 +0100

    [ADD] Testclass

--- bank/src/test/java/DBTests.java
+++ bank/src/test/java/DBTests.java
@@ -2,11 +2,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import bank.domain.Account;
+
 public class DBTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Assignment1Test() {
+		Account account = new Account(111L);
+		em.getTransaction().begin();
+		em.persist(account);
+		//TODO: verklaar en pas eventueel aan
+		assertNull(account.getId());
+		em.getTransaction().commit();
+		System.out.println(“AccountId: “ + account.getId());
+		//TODO: verklaar en pas eventueel aan
+		assertTrue(account.getId() > 0L);
+
 	}
 
 }
